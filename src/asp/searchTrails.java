b68aef6dba1defe34471ca09c557e269922a6824
Robert Kacir <kacir@wisc.edu>
2019-03-15 21:57:50 -0500

    example html added with improved search trail route

--- src/asp/searchTrails.java
+++ src/asp/searchTrails.java
@@ -1,5 +1,9 @@
 package asp;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @WebServlet("/searchTrails")
 public class searchTrails extends HttpServlet {
@@ -15,42 +20,96 @@ public class searchTrails extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //set the response characteristics
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
         //collect the parameters from front end
-        String foot = request.getParameter("foot");
-        String bike = request.getParameter("bike");
-        String water = request.getParameter("water");
+        Boolean foot = Boolean.parseBoolean(request.getParameter("foot"));
+        Boolean bike = Boolean.parseBoolean(request.getParameter("bike"));
+        Boolean water = Boolean.parseBoolean(request.getParameter("water"));
 
         String whereClause = "WHERE ";
-        if (!(foot == "") || !(bike == "") || !(water == "")) {
-            if (!(foot == "")){
-                whereClause += "design_cat = 'foot'";
+
+        if (foot == false && bike == false && water == false) {
+            //nothing is needed at all
+            whereClause = "";
+        } else {
+            if (foot){
+                whereClause += " design_cat = 'foot' ";
             }
-            if (!(bike == "")){
-                if (whereClause == "WHERE "){
-                    whereClause += "design_cat = 'bike'";
+            if (bike){
+                if (whereClause.length() == 0) {
+                    whereClause += " design_cat = 'bike' ";
                 } else {
-                    whereClause += "OR design_cat = 'bike'";
+                    whereClause += " OR design_cat = 'bike' ";
                 }
             }
-            if (!(water == "")){
-                if (whereClause == "WHERE "){
-                    whereClause += "design_cat = 'water'";
-                } else {
-                    whereClause += "OR design_cat = 'water'";
+            if (water) {
+                if (whereClause.length() == 0) {
+                    whereClause += " design_cat = 'water' ";
+                }
+                whereClause += " OR design_cat = 'water' ";
+            }
+            whereClause = " ( " + whereClause + ") ";
+        }
+
+
+
+        Boolean natural = Boolean.parseBoolean(request.getParameter("natural"));
+        Boolean paved = Boolean.parseBoolean(request.getParameter("paved"));
+        String surfaceSQL = "";
+        if (natural == false && paved == false){
+            //do nothing, no query to be added to SQL
+        } else {
+
+            if (natural){
+                surfaceSQL += " material = 'Natural' ";
+                if (paved){
+                    surfaceSQL += " OR material = 'Paved' ";
+                }
+            } else {
+                if (paved){
+                    surfaceSQL += " material = 'Paved' ";
                 }
             }
+
+            whereClause = whereClause + " AND ( " + surfaceSQL + " ) ";
         }
 
-        String natural = request.getParameter("natural");
-        String paved = request.getParameter("paved");
-        if (!(natural == "")){
-            whereClause += "material = 'Natural'";
-        }
+        String sql = "SELECT geom, name, design_cat, material FROM recreation_trail_line " + whereClause + ";";
+        System.out.println("entire SQL string has been generated. it is the following");
+        System.out.println(sql);
 
-        String sql = "SELECT geom, name, design_cat, material FROM recreation_trail_line";
+
+        JSONArray list = new JSONArray();
 
         DBUtility dbutil = new DBUtility();
         ResultSet res = dbutil.queryDB(sql);
+        try {
+            while (res.next()) {
+                JSONObject geometry = new JSONObject(res.getString("geom"));
+                JSONObject properties = new JSONObject();
+                properties.put("name" , res.getString("name"));
+                properties.put("design_cat" , res.getString("design_cat"));
+                properties.put("material" , res.getString("material"));
+
+                JSONObject masterObject = new JSONObject();
+                masterObject.put("type" , "Feature");
+                masterObject.put("geometry" , geometry);
+                masterObject.put("properties" , properties);
+
+                list.put(masterObject);
+            }
+
+            response.getWriter().write(list.toString());
+
+        } catch (SQLException e) {
+            System.out.println("SQL exception occured");
+        } catch (JSONException e) {
+            System.out.println("JSON exception Occured");
+        }
+
 
     }
 }
